753d2ef110bb3e0e0284869066a94a89d62749a7
tydaeus <5482721+tydaeus@users.noreply.github.com>
2021-08-29 22:25:12 -0500

    extract sprite placeholder code

--- experimentation.py
+++ experimentation.py
@@ -6,6 +6,8 @@ try:
 except ImportError:
     raise ImportError("NumPy and Surfarray are required.")
 
+import sprites
+
 pygame.init()
 
 # setup
@@ -19,35 +21,13 @@ black = 0, 0, 0
 white = 255, 255, 255
 screen.fill(white)
 
-# TODO: setup sprite(s)
-
-r = red = pygame.Color(255, 0, 0, 255)
-c = clear = pygame.Color(0, 0, 0, 0)
-
 squareArr = np.zeros((128, 128, 3), np.int32)
 print("squareArr:")
 print(squareArr)
 squareSurface = surfarray.make_surface(squareArr)
 screen.blit(squareSurface, squareSurface.get_rect())
 
-testSymbolSurface = pygame.Surface((10, 10), flags=pygame.SRCALPHA)
-testSymbolPxArray = pygame.PixelArray(testSymbolSurface)
-
-testSymbolPxArray[:,0:1] = [c, r, r, r, r, r, r, r, r, c]
-testSymbolPxArray[:,1:2] = [r, c, c, c, c, c, c, c, c, r]
-testSymbolPxArray[:,2:3] = [r, c, c, c, c, c, c, c, c, r]
-testSymbolPxArray[:,3:4] = [r, c, c, c, c, c, c, c, c, r]
-testSymbolPxArray[:,4:5] = [r, c, c, c, c, c, c, c, c, r]
-testSymbolPxArray[:,5:6] = [r, c, c, c, c, c, c, c, c, r]
-testSymbolPxArray[:,6:7] = [r, c, c, c, c, c, c, c, c, r]
-testSymbolPxArray[:,7:8] = [r, c, c, c, c, c, c, c, c, r]
-testSymbolPxArray[:,8:9] = [r, c, c, c, c, c, c, c, c, r]
-testSymbolPxArray[:,9:10] = [c, r, r, r, r, r, r, r, r, c]
-
-print("testSymbolPxArray:")
-print(testSymbolPxArray)
-del testSymbolPxArray
-screen.blit(testSymbolSurface, (120, 120))
+screen.blit(sprites.get_test_surface(), (120, 120))
 
 # loop
 while True:
---
+++ sprites.py
@@ -0,0 +1,23 @@
+import pygame, pygame.surfarray as surfarray
+
+r = red = pygame.Color(255, 0, 0, 255)
+c = clear = pygame.Color(0, 0, 0, 0)
+
+def get_test_surface():
+    testSymbolSurface = pygame.Surface((10, 10), flags=pygame.SRCALPHA)
+    testSymbolPxArray = pygame.PixelArray(testSymbolSurface)
+
+    testSymbolPxArray[:,0:1] = [c, r, r, r, r, r, r, r, r, c]
+    testSymbolPxArray[:,1:2] = [r, c, c, c, c, c, c, c, c, r]
+    testSymbolPxArray[:,2:3] = [r, c, c, c, c, c, c, c, c, r]
+    testSymbolPxArray[:,3:4] = [r, c, c, c, c, c, c, c, c, r]
+    testSymbolPxArray[:,4:5] = [r, c, c, c, c, c, c, c, c, r]
+    testSymbolPxArray[:,5:6] = [r, c, c, c, c, c, c, c, c, r]
+    testSymbolPxArray[:,6:7] = [r, c, c, c, c, c, c, c, c, r]
+    testSymbolPxArray[:,7:8] = [r, c, c, c, c, c, c, c, c, r]
+    testSymbolPxArray[:,8:9] = [r, c, c, c, c, c, c, c, c, r]
+    testSymbolPxArray[:,9:10] = [c, r, r, r, r, r, r, r, r, c]
+
+    del testSymbolPxArray
+
+    return testSymbolSurface
