d7771f8110456aa75154ac981ed1188af42c4f04
tydaeus <5482721+tydaeus@users.noreply.github.com>
2021-09-01 12:54:16 -0500

    extract general function convert_color_array_to_surface

--- sprites.py
+++ sprites.py
@@ -18,20 +18,23 @@ test_image_src = [
     [c, b, b, b, b, b, b, b, b, b, c]
 ]
 
-def get_test_surface():
-    height = len(test_image_src)
-    width = len(test_image_src[0])
+def convert_color_array_to_surface(color_array):
+    height = len(color_array)
+    width = len(color_array[0])
 
     test_surface = pygame.Surface((width, height), flags=pygame.SRCALPHA)
     test_pxarr = pygame.PixelArray(test_surface)
 
     for y in range(height):
-        if len(test_image_src[y]) != width:
+        if len(color_array[y]) != width:
             raise UserWarning(
-                f"Image array inconsistent row width; row 0: {width}px, row {y}: {len(test_image_src[y])}"
+                f"Image array inconsistent row width; row 0: {width}px, row {y}: {len(color_array[y])}"
             )
         for x in range(width):
-            test_pxarr[x,y] = test_image_src[y][x]
+            test_pxarr[x,y] = color_array[y][x]
 
     del test_pxarr
     return test_surface
+
+def get_test_surface():
+    return convert_color_array_to_surface(test_image_src)
