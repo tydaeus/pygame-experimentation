5ac254df5e576a996fbe1e59a77d28c4531e4831
tydaeus <5482721+tydaeus@users.noreply.github.com>
2021-09-01 12:44:40 -0500

    read image from 2d array

--- sprites.py
+++ sprites.py
@@ -2,23 +2,36 @@ import pygame, pygame.surfarray as surfarray
 
 r = red = pygame.Color(255, 0, 0, 255)
 c = clear = pygame.Color(0, 0, 0, 0)
+b = blue = pygame.Color(0, 0, 255, 255)
+
+test_image_src = [
+    [c, r, r, r, r, r, r, r, r, r, c],
+    [r, c, c, c, c, c, c, c, c, c, r],
+    [r, c, c, c, c, r, c, c, c, c, r],
+    [r, c, c, c, c, c, r, c, c, c, r],
+    [r, c, c, c, c, c, c, r, c, c, r],
+    [r, c, r, r, r, r, r, r, r, c, r],
+    [r, c, c, c, c, c, c, r, c, c, r],
+    [r, c, c, c, c, c, r, c, c, c, r],
+    [r, c, c, c, c, r, c, c, c, c, r],
+    [r, c, c, c, c, c, c, c, c, c, r],
+    [c, b, b, b, b, b, b, b, b, b, c]
+]
 
 def get_test_surface():
-    testSymbolSurface = pygame.Surface((11, 11), flags=pygame.SRCALPHA)
-    testSymbolPxArray = pygame.PixelArray(testSymbolSurface)
+    height = len(test_image_src)
+    width = len(test_image_src[0])
 
-    testSymbolPxArray[:,0:1] =      [c, r, r, r, r, r, r, r, r, r, c]
-    testSymbolPxArray[:,1:2] =      [r, c, c, c, c, c, c, c, c, c, r]
-    testSymbolPxArray[:,2:3] =      [r, c, c, c, c, r, c, c, c, c, r]
-    testSymbolPxArray[:,3:4] =      [r, c, c, c, c, c, r, c, c, c, r]
-    testSymbolPxArray[:,4:5] =      [r, c, c, c, c, c, c, r, c, c, r]
-    testSymbolPxArray[:,5:6] =      [r, c, r, r, r, r, r, r, r, c, r]
-    testSymbolPxArray[:,6:7] =      [r, c, c, c, c, c, c, r, c, c, r]
-    testSymbolPxArray[:,7:8] =      [r, c, c, c, c, c, r, c, c, c, r]
-    testSymbolPxArray[:,8:9] =      [r, c, c, c, c, r, c, c, c, c, r]
-    testSymbolPxArray[:,9:10] =     [r, c, c, c, c, c, c, c, c, c, r]
-    testSymbolPxArray[:,10:11] =    [c, r, r, r, r, r, r, r, r, r, c]
+    test_surface = pygame.Surface((width, height), flags=pygame.SRCALPHA)
+    test_pxarr = pygame.PixelArray(test_surface)
 
-    del testSymbolPxArray
+    for y in range(height):
+        if len(test_image_src[y]) != width:
+            raise UserWarning(
+                f"Image array inconsistent row width; row 0: {width}px, row {y}: {len(test_image_src[y])}"
+            )
+        for x in range(width):
+            test_pxarr[x,y] = test_image_src[y][x]
 
-    return testSymbolSurface
+    del test_pxarr
+    return test_surface
