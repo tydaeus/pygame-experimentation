e030675816220003e3a15b3cc93aedb68bc87be1
tydaeus <5482721+tydaeus@users.noreply.github.com>
2021-09-02 00:05:50 -0500

    better naming

--- sprites.py
+++ sprites.py
@@ -115,8 +115,8 @@ def convert_color_array_to_surface(color_array):
     height = len(color_array)
     width = len(color_array[0])
 
-    test_surface = pygame.Surface((width, height), flags=pygame.SRCALPHA)
-    test_pxarr = pygame.PixelArray(test_surface)
+    surface = pygame.Surface((width, height), flags=pygame.SRCALPHA)
+    pxarr = pygame.PixelArray(surface)
 
     for y in range(height):
         if len(color_array[y]) != width:
@@ -124,10 +124,10 @@ def convert_color_array_to_surface(color_array):
                 f"Image array inconsistent row width; row 0: {width}px, row {y}: {len(color_array[y])}"
             )
         for x in range(width):
-            test_pxarr[x,y] = color_array[y][x]
+            pxarr[x,y] = color_array[y][x]
 
-    del test_pxarr
-    return test_surface
+    del pxarr
+    return surface
 
 def get_test_surface():
     return convert_color_array_to_surface(convert_text_image_to_colorarray(test_txt_image))
