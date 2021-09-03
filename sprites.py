4c4a16b2c602f2611415d160ce085115be3ee861
tydaeus <5482721+tydaeus@users.noreply.github.com>
2021-09-02 22:12:21 -0500

    internalize image_identifier

--- sprites.py
+++ sprites.py
@@ -5,7 +5,7 @@ import pygame, image_loader
 class TestSprite(pygame.sprite.Sprite):
     def __init__(self, *args):
         pygame.sprite.Sprite.__init__(self, *args)
-        self.image_identifier = None
+        self._image_identifier = None
         self.image = None
         self.rect = pygame.Rect(0, 0, 0, 0)
 
@@ -14,8 +14,8 @@ class TestSprite(pygame.sprite.Sprite):
         pass
 
     def set_image_identifier(self, identifier):
-        if self.image_identifier != identifier:
-            self.image_identifier = identifier
+        if self._image_identifier != identifier:
+            self._image_identifier = identifier
             self.image = image_loader.load_image(identifier)
             self.rect.size = self.image.get_width(), self.image.get_height()
     
