58075f42ee0fd29ff4a35093931a2b90e357e98b
tydaeus <5482721+tydaeus@users.noreply.github.com>
2021-09-05 00:00:42 -0500

    add a separate sprite

--- sprites.py
+++ sprites.py
@@ -10,21 +10,11 @@ class _TestSprite(pygame.sprite.Sprite):
         self.rect = pygame.Rect(0, 0, 0, 0)
         self._pxsize = 100, 100
         self._current_message = 0
+        self.update_strategy = None
 
     def update(self):
-        vector = [0, 0]
-
-        if self._current_message.move_north:
-            vector[1] += -1
-        if self._current_message.move_east:
-            vector[0] += 1
-        if self._current_message.move_south:
-            vector[1] += 1
-        if self._current_message.move_west:
-            vector[0] += -1
-
-        if vector != [0, 0]:
-            self.rect.move_ip(vector[0], vector[1])
+        if callable(self.update_strategy):
+            self.update_strategy(self)
 
     def set_image_identifier(self, identifier):
         if self._image_identifier != identifier:
@@ -39,15 +29,37 @@ class _TestSprite(pygame.sprite.Sprite):
 
     def input(self, message):
         self._current_message = message
-    
 
 
 def init():
     global all_sprites
     all_sprites = pygame.sprite.Group()
 
-    testSprite = _TestSprite(all_sprites)
-    testSprite.set_image_identifier('test')
-    testSprite.rect.topleft = 120, 120
+    # set up player
+    playerSprite = _TestSprite(all_sprites)
+    playerSprite.set_image_identifier('test')
+    playerSprite.rect.topleft = 120, 120
+    
+    def _playerupdate(self):
+        vector = [0, 0]
 
-    input.add_subscriber(testSprite.input)
+        if self._current_message.move_north:
+            vector[1] += -1
+        if self._current_message.move_east:
+            vector[0] += 1
+        if self._current_message.move_south:
+            vector[1] += 1
+        if self._current_message.move_west:
+            vector[0] += -1
+
+        if vector != [0, 0]:
+            self.rect.move_ip(vector[0], vector[1])
+
+    playerSprite.update_strategy = _playerupdate
+
+    input.add_subscriber(lambda message: playerSprite.input(message))
+
+    # set up block
+    blockSprite = _TestSprite(all_sprites)
+    blockSprite.set_image_identifier('block')
+    blockSprite.rect.topleft = 500, 500
